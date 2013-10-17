25a088afa78a44f3472e7282eb204a098ee732d3
Eleanorw <eleanorwong@Eleanors-MacBook-Pro.local>
2013-10-17 00:24:12 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,26 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name:Eleanor Wong
+
+#2.1 
+
+#2.2
+#	print 5
+#	print 'x = 5'
+#	print x + 1
+#	output is 6
+
+
+#2.3
+#	1. 8, int
+#	2. 8.5, float
+#	3. 4.0, float
+#	4. 11, int
+#	5. '.....', string
+
+#2.4
+#	pi is an operand that is not an int therefore changing the other ints to float will become more accurate
+#	pi is 3.1415926535897931
+#	r = 5 changes to r = 5.0
+#	4.0/3.0*pi*r**3
+#	523.5987755982989 
